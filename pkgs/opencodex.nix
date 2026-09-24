fa42f4832943b3dfb6950aae3c9676ebb7cad6d5
pexisgle <248136655+pexisgle@users.noreply.github.com>
2026-09-24 18:05:51 +0000

    chore(update): apply automated updates

--- pkgs/opencodex.nix
+++ pkgs/opencodex.nix
@@ -10,11 +10,11 @@
 
 stdenv.mkDerivation {
   pname = "opencodex";
-  version = "2.63.0";
+  version = "2.64.0";
 
   src = fetchzip {
-    url = "https://registry.npmjs.org/@bitkyc08/opencodex/-/opencodex-2.63.0.tgz";
-    hash = "sha256-ydD2n2teagDSI+qWBVwvkONfWbtwmdm5V9485gNRnYc=";
+    url = "https://registry.npmjs.org/@bitkyc08/opencodex/-/opencodex-2.64.0.tgz";
+    hash = "sha256-vkz7YzPy4K4j63aEhQ9UF+NbagDDy0pLUEz0g0euuVY=";
   };
 
   npmDeps = importNpmLock.buildNodeModules {
